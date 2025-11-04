8e389c70627975d0baf2cf68e66336daab70f45e
sykim <sykim@localhost.localdomain>
2025-11-04 17:32:28 +0900

    server port

--- main.py
+++ main.py
@@ -147,4 +147,4 @@ def get_live_status():
     return get_live_status_sync()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
